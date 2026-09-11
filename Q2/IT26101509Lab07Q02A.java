ca4eb12f941d6ca3fb128a87210b3f7803468ea8
IT26101509 <IT26101509@my.sliit.lk>
2026-09-11 22:36:41 +0530

    IT26101509Lab07Q02.java to Q2

---
+++ Q2/IT26101509Lab07Q02A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IT26101509Lab07Q02A
+{
+    public static void main(String[] args) {
+		
+		int rows = 4;
+		int columns = 5;
+		
+		for (int rowCount = 0; rowCount < rows; rowCount++) {
+			
+			for (int columnCount = 0; columnCount < columns; columnCount++){
+				System.out.print("$ ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
